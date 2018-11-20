c833624fe69a7d3b551f43e7becd8c0fd0c59a97
jedijackatack <jedijackattack@gmail.com>
2018-11-20 10:04:12 +0000

    yea

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import Code.Component as Component
 import Code.Entity as Entity
 import Code.Managers as Managers
+import Code.Simulation as Simulation
 import os
 import xml.etree.ElementTree
 pygame.init()
@@ -48,14 +49,20 @@ basepath = os.path.dirname(os.path.realpath(__file__))
 print(basepath)
 fullpath = os.path.join(basepath,"Saves\CORE\Battles\Test1.xml")
 print(fullpath)
-
+"""
 tree = xml.etree.ElementTree.parse(fullpath)
 battle = tree.getroot()
 
 for child in battle:
-    print(child.tag)
+    print(child.tag,":",child.text)
     for element in child:
         print(element.tag,":",element.text)
+        if(len(element) >= 2):
+            for data in element:
+                print(data.tag,":",data.text)
 
 
 print(battle[0].text)
+"""
+s = Simulation.Simulation("Test1.xml")
+s.CreateTankManagerTypes()
